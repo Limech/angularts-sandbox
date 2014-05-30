e5e8b7f568c6ac33065caa36e3d56d808546c284
webocular <webocular@gmail.com>
2014-05-30 02:11:13 -0400

    added routing example

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,6 +1,7 @@
 'use strict';
 
-var sandboxApp: ng.IModule = angular.module('sandboxApp', [])
+var sandboxApp: ng.IModule = angular.module('sandboxApp', ['ngRoute'])
+    .config(RouteCtrl)
     .service('myFeatureSvc', MyFeatureSvc)
     .controller('myFeatureOneCtrl', MyFeatureCtrl)
     .controller('myFeatureTwoCtrl', MyFeatureCtrl);
